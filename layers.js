b4a092a7ea1ed3eef344dfa06a7b56df3a0e58be
Karina Torres <torresk@ufl.edu>
2025-03-16 01:59:14 +0000

    fixed popups and image layer issue

--- layers.js
+++ layers.js
@@ -1,27 +1,64 @@
-define(["esri/layers/FeatureLayer"], (FeatureLayer) => {
+define(["esri/layers/FeatureLayer", 
+    "esri/layers/ImageryLayer",
+    "esri/renderers/SimpleRenderer",
+    "esri/symbols/SimpleMarkerSymbol"],
+    (FeatureLayer, ImageryLayer, SimpleRenderer, SimpleMarkerSymbol) => {
     
     // ---------- ADD SOCIAL INFORMATIONAL DATASETS ----------
     const padusLayer = new FeatureLayer({
-        url: "https://services.arcgis.com/v01gqwM5QqNysAAi/arcgis/rest/services/Manager_Name_PADUS/FeatureServer"
+        url: "https://services.arcgis.com/v01gqwM5QqNysAAi/arcgis/rest/services/Manager_Name_PADUS/FeatureServer",
+        title: "PADUS"
     });
 
     const wuiLayer = new ImageryLayer({
-        url: "https://tiledimageservices.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/Global_WUI_5/ImageServer"
+        url: "https://tiledimageservices.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/Global_WUI_5/ImageServer",
+        title: "WUI"
     });
 
-
-    
     // ---------- ADD COGONGRASS DATASETS ----------
     const inatCogongrassLayer = new FeatureLayer({
-        url: "https://services.arcgis.com/LBbVDC0hKPAnLRpO/arcgis/rest/services/iNaturalist_Cogongrass_Observations/FeatureServer"
+        url: "https://services.arcgis.com/LBbVDC0hKPAnLRpO/arcgis/rest/services/iNaturalist_Cogongrass_Observations/FeatureServer",
+        title: "iNaturalist Cogongrass",
+        renderer: new SimpleRenderer({
+            symbol: new SimpleMarkerSymbol({
+                color: "red",
+                size: "8px",
+                outline: {
+                    color: "white",
+                    width: 1
+                }
+            })
+        })
     });
 
     const eddmapsCogongrassLayer = new FeatureLayer({
-        url: "https://services.arcgis.com/LBbVDC0hKPAnLRpO/arcgis/rest/services/EDDmapS_Cogongrass_Observations/FeatureServer"
+        url: "https://services.arcgis.com/LBbVDC0hKPAnLRpO/arcgis/rest/services/EDDmapS_Cogongrass_Observations/FeatureServer",
+        title: "EDDmapS Cogongrass",
+        renderer: new SimpleRenderer({
+            symbol: new SimpleMarkerSymbol({
+                color: "blue",
+                size: "8px",
+                outline: {
+                    color: "white",
+                    width: 1
+                }
+            })
+        })
     });
 
     const survey123CogongrassLayer = new FeatureLayer({
-        url: "https://services.arcgis.com/LBbVDC0hKPAnLRpO/arcgis/rest/services/survey123_3f14c6513de74675844472a86bbee17f_results/FeatureServer"
+        url: "https://services.arcgis.com/LBbVDC0hKPAnLRpO/arcgis/rest/services/survey123_3f14c6513de74675844472a86bbee17f_results/FeatureServer",
+        title: "survey123 Cogongrass",
+        renderer: new SimpleRenderer({
+            symbol: new SimpleMarkerSymbol({
+                color: "green",
+                size: "8px",
+                outline: {
+                    color: "white",
+                    width: 1
+                }
+            })
+        })
     });
 
     return {
--- popups.js
+++ popups.js
@@ -1,23 +1,27 @@
 define([], () => {
+    
+    // ----------  SOCIAL INFORMATIONAL DATASETS ----------
     const padusPopupTemplate = {
-        title: "Protected Area: {NAME}",
-        content: "This is a protected area managed by {MANAGER}."
+        title: "Protected Area: {Loc_Nm}",
+        content: "This is a protected area managed by {Loc_Mang}."
     };
 
-    
-    const wuiPopupTemplate = {
-        title: "WUI Area: {NAME}",
-        content: "This area is part of the Wildland Urban Interface."
-    };
 
-    const cogongrassPopupTemplate = {
+
+    // ----------  COGONGRASS DATASETS ----------
+    const iNatPopupTemplate = {
         title: "Cogongrass Occurrence",
-        content: "Reported by {REPORTER} on {DATE}."
+        content: "Reported by {user_name} on {observed_on}."
     };
     
+    const eddmapsPopupTemplate = {
+        title: "Cogongrass Occurrence",
+        content: "Reported by {reporter} on {ObsDate}."
+    };
+
     const survey123CogongrassPopupTemplate = {
         title: "Cogongrass Survey",
-        content: "Reported by {report_name} on {date_time}."
+        content: "Reported by {reporter_name} on {date_time}."
     };
 
     return {
@@ -25,10 +29,12 @@ define([], () => {
             view.map.layers.forEach(layer => {
                 if (layer.title === "PADUS") {
                     layer.popupTemplate = padusPopupTemplate;
-                } else if (layer.title === "WUI") {
-                    layer.popupTemplate = wuiPopupTemplate;
-                } else if (layer.title.includes("Cogongrass")) {
-                    layer.popupTemplate = cogongrassPopupTemplate;
+                } else if (layer.title === "iNaturalist Cogongrass") {
+                    layer.popupTemplate = iNatPopupTemplate;
+                } else if (layer,title === "EDDmapS Cogongrass") {
+                    layer.popupTemplate = eddmapsPopupTemplate;
+                } else if (layer.title.includes("survey123")) {
+                    layer.popupTemplate = survey123CogongrassPopupTemplate;
                 }
             });
         }
